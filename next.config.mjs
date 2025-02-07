90c7ac4c2e60c6e0d40641c04e9f6af4bc0d1cbc
Álvaro Mañoso Oca <alvaro.manoso@students.salle.url.edu>
2025-02-07 21:13:14 +0100

    Permanent site redirection to new domain alvaromanoso.dev

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: "/",
+                destination: "https://alvaromanoso.dev",
+                permanent: true,
+            },
+        ];
+    }
+};
 
 export default nextConfig;
